8489354340ec99f0885f2036b6176b5ab44f0aed
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-06-30 11:50:58 +0545

    Updated

--- frontend/src/components/Header/header.js
+++ frontend/src/components/Header/header.js
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" bg="primary" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">NoteKrator</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className='m-auto'>
@@ -25,18 +25,10 @@ const Header = () => {
             </Form>
             </Nav>
           <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+            <Nav.Link href="#home">My Notes</Nav.Link>
+            <NavDropdown title="Ashfaque Hossain" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
