955f44b7a33da1d392f8dca13ce9b14e0ff944c7
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-06-30 12:05:36 +0545

    Updated

--- frontend/src/components/Footer/footer.js
+++ frontend/src/components/Footer/footer.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Container from 'react-bootstrap'
+import Col from 'react-bootstrap'
+import Row from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <footer 
+    style={{
+      width: "100%",
+      position: "relative",
+      bottom: 0,
+      display: "flex",
+      justifyContent: "center",
+    }}
+    >
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; NoteKrator</Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
