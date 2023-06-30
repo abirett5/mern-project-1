305c423c9bb80f4f834263aafe34a11906fbdd93
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-06-30 12:19:13 +0545

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Footer from './components/Footer/footer';
 import Header from './components/Header/header';
-
+import LandingPage from './screens/LandingPage/LandingPage';
 
 const App = () => (
     <>
     <Header />
     <main style={{minHeight:"93vh"}}></main>
+    <LandingPage />
 
     <Footer />
     </>
