852e505831ed8e77aaba7fba7bcbee2f3420cb9c
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-06-30 10:30:40 +0545

    Files added

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './components/Footer/footer';
+import Header from './components/Header/header';
+
+
+const App = () => (
+    <>
+    <Header />
+    <main style={{minHeight:"93vh"}}></main>
+
+    <Footer />
+    </>
+)
+
+export default App;
