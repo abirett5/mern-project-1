fdf2531062ba3219b3e349829eb22ca9e32a9c84
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-07-01 12:32:35 +0545

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,19 @@ import './App.css';
 import Footer from './components/Footer/footer';
 import Header from './components/Header/header';
 import LandingPage from './screens/LandingPage/LandingPage';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MyNotes from './screens/MyNotes/MyNotes';
 
 const App = () => (
-    <>
+    <BrowserRouter>
     <Header />
     <main>
-        <LandingPage />
+        <Route path='/' Component={LandingPage}/>
+        <Route path='/mynotes' Component={()=><MyNotes />}/>
     </main>
 
     <Footer />
-    </>
+    </BrowserRouter>
 )
 
 export default App;
-
-// Continue from 17:08
