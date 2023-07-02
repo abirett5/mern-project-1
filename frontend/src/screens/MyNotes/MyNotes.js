8fb826c4ec5f0e5810abbb81cc79eb2d7a86b40b
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-07-02 22:14:16 +0545

    Updated

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -5,9 +5,6 @@ import Button from 'react-bootstrap/esm/Button';
 import Card from 'react-bootstrap/Card';
 import notes from "../../data/notes";
 import Badge from 'react-bootstrap/Badge';
-import Accordion from 'react-bootstrap/Accordion';
-import AccordionHeader from 'react-bootstrap/AccordionHeader';
-import AccordionCollapse from 'react-bootstrap/AccordionCollapse';
 
 const MyNotes = () => {
   const deleteHandler = (id) => {
@@ -23,8 +20,7 @@ const MyNotes = () => {
       </Link>
         {
           notes.map(note => (
-          <Accordion>
-            <Card style={{ margin: 10 }}>
+          <Card style={{ margin: 10 }}>
             <Card.Header style={{ display: "flex" }}>
               <span style={{ 
                 color: "black",
@@ -34,8 +30,7 @@ const MyNotes = () => {
                 alignSelf: "center",
                 fontSize: 18,
               }}
-              ><AccordionHeader as={Card.Text} variant='link' eventKey='0'>{note.title}</AccordionHeader>
-              </span>
+              >{note.title}</span>
               
               <div>
                 <Button href={`/note/${note._id}`}>Edit</Button>
@@ -47,12 +42,10 @@ const MyNotes = () => {
                 </Button>
               </div>
             </Card.Header>
-            <AccordionCollapse eventKey='0'>
             <Card.Body>
               <h4>
                 <Badge style={{backgroundColor: "#28a745", color: "white"}}>Category - {note.category}</Badge>
               </h4>
-
               <blockquote className="blockquote mb-0">
                 <p>
                   {note.content}
@@ -60,9 +53,7 @@ const MyNotes = () => {
                 <footer className="blockquote-footer">Created On - date</footer>
               </blockquote>
             </Card.Body>
-            </AccordionCollapse>
             </Card>
-          </Accordion>
           ))
         }
     </MainScreen>
