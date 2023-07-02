2f05824fc33c30bfc5d1fb8d255822902fe550f6
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-07-02 17:06:48 +0545

    Updated

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -3,30 +3,35 @@ import MainScreen from '../../components/MainScreen';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/esm/Button';
 import Card from 'react-bootstrap/Card';
+import notes from "../../data/notes";
 
 const MyNotes = () => {
   return (
     <MainScreen title='Welcome back Ashfaque Hossain Abir...'>
       <Link to='createnote'>
         <Button style={{marginLeft:10, marginBottom:6}} size='lg'>Create New Note</Button>
-        <Card style={{ margin: 10 }}>
-          <Card.Header style={{ display: "flex" }}>
-            <span style={{ 
-              color: "black",
-              textDecoration: "none",
-              flex: 1,
-              cursor: "pointer",
-              alignSelf: "center",
-              fontSize: 18,
+        {
+          notes.map(note => (
+          <Card style={{ margin: 10 }}>
+            <Card.Header style={{ display: "flex" }}>
+              <span style={{ 
+                color: "black",
+                textDecoration: "none",
+                flex: 1,
+                cursor: "pointer",
+                alignSelf: "center",
+                fontSize: 18,
               }}
-            >Title</span>
-            
-            <div>
-              <Button>Edit</Button>
-              <Button variant='danger' className='mx-2'>Delete</Button>
-            </div>
-          </Card.Header>
-        </Card>
+              >{note.title}</span>
+              
+              <div>
+                <Button>Edit</Button>
+                <Button variant='danger' className='mx-2'>Delete</Button>
+                </div>
+                </Card.Header>
+                </Card>
+          ))
+        }
       </Link>
     </MainScreen>
   )
