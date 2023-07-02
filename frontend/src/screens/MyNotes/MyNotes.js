0759437b3ee6b3dcd95f962cb3302a2f4533c240
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2023-07-02 12:47:37 +0545

    Updated

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import MainScreen from '../../components/MainScreen';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/esm/Button';
 
 const MyNotes = () => {
   return (
-    <MainScreen title='Welcome back Ashfaque Hossain Abir...'>My Notes</MainScreen>
+    <MainScreen title='Welcome back Ashfaque Hossain Abir...'>
+      <Link to='createnote'>
+        <Button style={{marginLeft:10, marginBottom:6}} size='lg'>Create New Note</Button>
+      </Link>
+    </MainScreen>
   )
 }
 
